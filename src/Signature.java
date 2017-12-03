1adf82c0b2a95f8728bdf3a054539843c1b6ad65
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2017-12-03 21:03:29 +0000

    Finished

--- src/Signature.java
+++ src/Signature.java
@@ -26,6 +26,9 @@ public class Signature {
   static final String PRIMEMOD = "b59dd79568817b4b9f6789822d22594f376e6a9abc0241846de426e5dd8f6eddef00b465f38f509b2b18351064704fe75f012fa346c5e2c442d7c99eac79b2bc8a202c98327b96816cb8042698ed3734643c4c05164e739cb72fba24f6156b6f47a7300ef778c378ea301e1141a6b25d48f1924268c62ee8dd3134745cdf7323";
   static final String GENERATOR = "44ec9d52c8f9189e49cd7c70253c2eb3154dd4f08467a64a0267c9defe4119f2e373388cfa350a4e66e432d638ccdc58eb703e31d4c84e50398f9f91677e88641a2d2f6157e2f4ec538088dcf5940b053c622e53bab0b4e84b1465f5738f549664bd7430961d3e5a2e7bceb62418db747386a58ff267a9939833beefb7a6fd68";
 
+  static final BigInteger i0 = BigInteger.ZERO;
+  static final BigInteger i1 = BigInteger.ONE;
+
   public static void main(String[] args) {
     if(args.length < 1) {
       throw new RuntimeException("file path required");
@@ -33,66 +36,89 @@ public class Signature {
 
     BigInteger p = new BigInteger(PRIMEMOD, 16);
     BigInteger g = new BigInteger(GENERATOR, 16);
-    BigInteger x = RandGen.inRange(BigInteger.ONE, p.subtract(BigInteger.ONE));
+    BigInteger h = hash(readFile(args[0]));
 
-    BigInteger pubKey = modpow(g, x, p);
+    // Helpers
+    BigInteger pm1 = p.subtract(i1);
 
-    System.out.println("PUBLIC KEY:\n" + pubKey.toString(16));
+    BigInteger x = RandGen.inRange(i0, pm1);
 
-    BigInteger m = readFile(args[0]);
-    BigInteger h = hash(m);
+    BigInteger y = modpow(g, x, p);
 
-    BigInteger r;
-    BigInteger s;
-    do {
+    BigInteger r = BigInteger.ZERO;
+    BigInteger s = BigInteger.ZERO;
+    while (s.equals(i0)) {
       BigInteger k;
-      BigInteger tmpGcd;
       do {
-        k = RandGen.inRange(BigInteger.ZERO, p.subtract(BigInteger.ONE));
-        
-        //TODO: remove this printing
-        tmpGcd = gcd(k, p.subtract(BigInteger.ONE));
-        System.out.println(tmpGcd.toString());
-      } while ( tmpGcd.compareTo(BigInteger.ONE) != 0 );
+        k = RandGen.inRange(i0, pm1);
+      } while(!gcd(k, pm1).equals(i1));
 
       r = modpow(g, k, p);
-      s = modinv(h.subtract(x.multiply(r)), k).mod(p.subtract(BigInteger.ONE));
 
-    } while (s.compareTo(BigInteger.ZERO) == 0);
+      BigInteger xr = x.multiply(r);
 
+      s = h.subtract(xr).multiply(modinv(k, pm1)).mod(pm1);
+    }
 
     // FINAL CHECKS
-    if (r.max(BigInteger.ZERO).compareTo(r.min(p)) != 0) {
+    if (!r.max(i0).equals(r.min(p))) {
       throw new RuntimeException("not 0 < r < p");
     }
-    if (s.max(BigInteger.ZERO).compareTo(s.min(p.subtract(BigInteger.ONE))) != 0) {
+    if (!s.max(i0).equals(s.min(pm1))) {
       throw new RuntimeException("not 0 < s < p-1");
     }
 
-    // TODO: Perform signature check (maybe)
-    // BigInteger v1 = modpow(pubKey, r , new BigInteger);
-    // BigInteger v2 = modpow(r, s, r.add(BigInteger.ONE));
-    // if(modpow(g, h, p).compareTo((v1.multiply(v2)).mod(p)) != 0 ) {
-    //   throw new RuntimeException("not a valid signature");
-    // }
+    BigInteger v1 = modpow(y, r, p);
+    BigInteger v2 = modpow(r, s, p);
 
-    System.out.println("R:\n" + r.toString(16));
-    System.out.println("S:\n" + s.toString(16));
+    BigInteger left = modpow(g, h, p);
+    BigInteger right = (v1.multiply(v2)).mod(p);
+
+    if(!left.equals(right)) {
+      throw new RuntimeException("not a valid signature");
+    }
+
+    System.out.println("PUBLIC KEY:\n" + y.toString(16));
+    System.out.println("\nR:\n" + r.toString(16));
+    System.out.println("\nS:\n" + s.toString(16));
   }
 
-  // TODO: Own implementation
   static BigInteger modpow(BigInteger n, BigInteger e, BigInteger m) {
     return n.modPow(e, m);
   }
 
-  // TODO: Own implementation
   static BigInteger gcd(BigInteger a, BigInteger b) {
-    return a.gcd(b);
+    return xgcd(a,b)[0];
   }
 
-  // TODO: Own implementation
   static BigInteger modinv(BigInteger i, BigInteger m) {
-    return i.modInverse(m);
+    return xgcd(i, m)[1].mod(m);
+  }
+
+  static BigInteger[] xgcd(BigInteger a, BigInteger b) {
+  	BigInteger[] qr = new BigInteger[2];
+
+  	BigInteger x = BigInteger.ONE, lastX = BigInteger.ZERO;
+  	BigInteger y = BigInteger.ZERO, lastY = BigInteger.ONE;
+
+    BigInteger r = BigInteger.ZERO, xr = BigInteger.ZERO;
+  	while (true){
+  	    qr = a.divideAndRemainder(b); r = qr[0]; a = qr[1];
+  	    x = x.subtract(y.multiply(r));
+  	    lastX = lastX.subtract(lastY.multiply(r));
+  	    if (a.equals(BigInteger.ZERO)) {
+          r = b; xr = y; break;
+        };
+  	    qr = b.divideAndRemainder(a); r = qr[0]; b = qr[1];
+  	    y = y.subtract(x.multiply(r));
+  	    lastY = lastY.subtract(lastX.multiply(r));
+  	    if (b.equals(BigInteger.ZERO)) {
+          r = a; xr = x; break;
+        };
+  	}
+
+    BigInteger[] res = {r, xr};
+    return res;
   }
 
   static BigInteger hash(BigInteger m) {
